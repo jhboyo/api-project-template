10c34c50936c4a3be26478595024d3f605a3d816
valeos <code.juno@gmail.com>
2023-12-31 23:47:58 +0900

    initial commit - hateoas setting

---
+++ src/main/java/com/example/payroll/sample/Employee.java
@@ -0,0 +1,24 @@
+package com.example.payroll.sample;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Employee {
+
+    private @Id
+    @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
